7a330d53977c539d0e7809b6141df27f7a96b865
hirotoarai <ah940325@gmail.com>
2021-01-10 19:48:19 +0000

    ヘッダーのリンク周りのエフェクト追加、フッダーロゴクリックで最上部に移動

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,3 +72,12 @@ $(function(){
 		return false;
 	});
 });
+
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
