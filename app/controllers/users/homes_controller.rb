3b66ce6a72c37d68b186c0743a656bb760511201
hirotoarai <ah940325@gmail.com>
2021-01-27 14:12:22 +0000

    ランキング機能をcustmerページからtopページに移動

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -3,6 +3,7 @@ class Users::HomesController < ApplicationController
   def top
     @posts = Post.all.page(params[:page]).per(10)
     @post = Post.all
+    @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 
   def about
