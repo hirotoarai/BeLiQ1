57687f2240ae9b54825e3a5edf27d08d077ce13c
hirotoarai <ah940325@gmail.com>
2021-02-17 06:02:20 +0000

    admin modelsの説明文を記載

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,6 +4,8 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable#, :validatable
 
+  # ユーザーネームは２文字以上、２０字以内
   validates :name, uniqueness: true,length: { maximum: 20, minimum: 2 }
+  # パスワードは８文字以上
   validates :encrypted_password,    length: { minimum: 8 }
 end
