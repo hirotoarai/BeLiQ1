7da022720d53ea8a924806c710bcbe6635fe7e78
hirotoarai <ah940325@gmail.com>
2021-02-25 02:37:28 +0000

    searchs modelsの説明文を記載

--- app/controllers/users/searchs_controller.rb
+++ app/controllers/users/searchs_controller.rb
@@ -4,6 +4,7 @@ class Users::SearchsController < ApplicationController
     #検索条件に一致したポストモデルの取得
     @posts = Post.search(params[:search], params[:type]).page(params[:page]).per(10)
     @post = Post.search(params[:search], params[:type])
+    # 検索条件で前件取得した場合
     @posts_all = @posts.all
     @post_all = @post.all
   end
