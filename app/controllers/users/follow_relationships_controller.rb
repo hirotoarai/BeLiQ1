8707a9153ac2fe3daac71dba37fdcbbd993c26c3
hirotoarai <ah940325@gmail.com>
2021-03-05 05:42:17 +0000

    follow_relationships_controllerの記述を修正2

--- app/controllers/users/follow_relationships_controller.rb
+++ app/controllers/users/follow_relationships_controller.rb
@@ -3,6 +3,7 @@ class Users::FollowRelationshipsController < ApplicationController
   def create
     # フォローリレーションシップテーブルからフォローid情報を取得
     @user = User.find(params[:follow_relationship][:following_id])
+    # フォローをする機能を追加
     current_user.follow(@user)
     respond_to do |format|
       format.html {redirect_back(fallback_location: root_url)}
@@ -13,6 +14,7 @@ class Users::FollowRelationshipsController < ApplicationController
   def destroy
     # フォローリレーションシップテーブルからフォローid情報を取得
     @user = User.find(params[:follow_relationship][:following_id])
+    # フォローを外す機能を追加
     current_user.unfollow(@user)
     respond_to do |format|
       format.html {redirect_back(fallback_location: root_url)}
