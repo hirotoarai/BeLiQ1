9065166a9811cac6bc0ad91a00fb7a6ec5848276
hirotoarai <ah940325@gmail.com>
2021-03-06 04:56:20 +0000

    home_controllerの説明文を追記

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -1,6 +1,7 @@
 class Users::HomesController < ApplicationController
 
   def top
+    # ポストテーブルの全ての投稿を表示
     @posts = Post.all.page(params[:page]).per(10)
     @post = Post.all
     @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
