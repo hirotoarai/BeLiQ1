6d938515c28ff212bb324795fa0ce1f3f307f280
hirotoarai <ah940325@gmail.com>
2021-03-07 04:38:36 +0000

    homes_controllerの説明文を追記2

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -1,9 +1,10 @@
 class Users::HomesController < ApplicationController
 
   def top
-    # ポストテーブルの全ての投稿を表示
+    # ポストテーブルの全ての投稿を取得
     @posts = Post.all.page(params[:page]).per(10)
     @post = Post.all
+    # いいねランキングを３位まで取得
     @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 
