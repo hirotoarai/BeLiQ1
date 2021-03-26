4d331a622ca3a61a573ec406178f3e32674a4aed
hirotoarai <ah940325@gmail.com>
2021-03-26 03:17:00 +0000

    posts_controllerの説明文を追記3

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -11,6 +11,7 @@ class Users::PostsController < ApplicationController
     @post = Post.find(params[:id])
     #投稿に関連するタグの取得
     @post_tags = @post.tags
+    # 投稿の空のインスタンスを作成
     @post_new = Post.new
     @post_comment = PostComment.new
     @post_comments = @post.post_comments.page(params[:page]).per(5)
